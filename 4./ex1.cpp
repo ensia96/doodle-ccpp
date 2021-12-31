b12465c55858689cc914d1872ad052283e0e1b3f
춤추는망고 <ensia96@gmail.com>
2021-12-31 15:40:48 +0900

    [add] source code - 4

---
+++ 4./ex1.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    // int : 정수 자료형 -> a라는 이름으로 32bit(4byte) 만큼 메모리 공간 할당
+    int a; // 선언
+
+    a = 3; // 대입
+    printf("%d\n", a);
+
+    a = 5; // 대입
+    printf("%d\n", a);
+}
