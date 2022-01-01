311fc43c97240b4a572c5284ca63f73702acefdd
춤추는망고 <ensia96@gmail.com>
2022-01-01 19:01:49 +0900

    [add] source code - 13

---
+++ 13./ex1.cpp
@@ -0,0 +1,37 @@
+// 중첩 if문
+// 중괄호 코딩 스타일
+
+#include <stdio.h>
+
+int main()
+{
+    int a, b, c;
+
+    scanf("%d%d%d", &a, &b, &c);
+
+    /*
+    if (a > b) {
+        if (a > c) {
+            printf("%d\n", a);
+        } else { // c > a
+            printf("%d\n", c);
+        }
+    } else { // b > a
+        if (b > c) {
+            printf("%d\n", b);
+        } else { // c > b
+            printf("%d\n", c);
+        }
+    }
+    */
+
+    if (a > b) {
+        if (a > c)
+            printf("%d\n", a);
+        else
+            printf("%d\n", c);
+    } else if (b > c)
+        printf("%d\n", b);
+    else
+        printf("%d\n", c);
+}
